5acdbe99917b8840851c46e325bd03744d8fb276
fannilin <103945012+fannilin@users.noreply.github.com>
2023-01-22 22:18:32 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import {  useState } from 'react';
 
 export default function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(number1 + number2);
+
+  function addTogether() {
+      const newResult = number1 + number2;
+      setResult(newResult);
+      Alert.alert('Alert', 'Tulos: ' + newResult);
+  }
+
+  function minusOther() {
+    const newResult = number1 - number2;
+    setResult(newResult);
+    Alert.alert('Alert', 'Tulos: ' + newResult);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+          <TextInput
+          styles={styles.input}
+              type="number"
+              placeholder="0"
+              value={number1}
+              onChangeText={v => {
+                  setNumber1(Number.parseInt(v)); 
+              }}
+          />
+          <TextInput
+          styles={styles.input}
+              type="number"
+              placeholder="0"
+              value={number2}
+              onChange={e => {
+                  setNumber2(Number.parseInt(e.nativeEvent.text)); 
+              }}
+          />
+
+          <Button onPress={addTogether} title="+" />
+          <Button onPress={minusOther} title="-" />
+      </View>
   );
 }
 
@@ -17,4 +52,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width:100  , 
+    borderColor: 'blue', 
+    borderWidth: 1
+  }
 });
